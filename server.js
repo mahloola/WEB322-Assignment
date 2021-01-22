1f267522516065d38bb3c08f92f2263cc819faf1
mahloola <mahloola08@gmail.com>
2021-01-22 12:09:58 -0500

    added honesty statement

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/********************************************************************************* 
+*  WEB322 – Assignment 1 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*  No part of this assignment has been copied manually or electronically from any other source 
+*  (including web sites) or distributed to other students. 
+*  
+*  Name: Mahmoud Bakir // Student ID: 127859197 // Date: January 22nd, 2021
+* 
+*  Online (Heroku) URL: https://web322-mahmoud.herokuapp.com/
+* 
+********************************************************************************/  
 
 const express = require("express");
 const app = express();
